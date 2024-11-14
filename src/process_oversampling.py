04990b86b9156b8afb6483f3c4d1798b8fe0def2
ward-benjamin <wardb@stanford.edu>
2024-11-14 15:29:56 -0800

    Modif_oversampling

--- src/process_oversampling.py
+++ src/process_oversampling.py
@@ -28,7 +28,16 @@ def oversample_train_test_SMOTE(df):
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.05,random_state=42)
     smote = SMOTE(random_state=42)
     X_train_resampled, y_train_resampled = smote.fit_resample(X_train,y_train)
-    return X_train_resampled, y_train_resampled, X_test, y_test
+
+    X_train_tensor = torch.tensor(X_train_resampled.values,dtype=torch.float32)
+    y_train_tensor = torch.tensor(y_train_resampled.values,dtype=torch.long)
+    train_dataset = TensorDataset(X_train_tensor,y_train_tensor)
+
+    X_test_tensor = torch.tensor(X_test.values,dtype=torch.float32)
+    y_test_tensor = torch.tensor(y_test.values,dtype=torch.long)
+    test_dataset = TensorDataset(X_test_tensor,y_test_tensor)
+
+    return train_dataset, test_dataset
 
 def undersample_train_test_NM(df):
     X = df.drop(columns=["Diabetes_status"])
@@ -36,4 +45,13 @@ def undersample_train_test_NM(df):
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.05,random_state=42)
     nearmiss = NearMiss(version=1)
     X_train_resampled, y_train_resampled = nearmiss.fit_resample(X_train,y_train)
-    return X_train_resampled, y_train_resampled, X_test, y_test
+    
+    X_train_tensor = torch.tensor(X_train_resampled.values,dtype=torch.float32)
+    y_train_tensor = torch.tensor(y_train_resampled.values,dtype=torch.long)
+    train_dataset = TensorDataset(X_train_tensor,y_train_tensor)
+
+    X_test_tensor = torch.tensor(X_test.values,dtype=torch.float32)
+    y_test_tensor = torch.tensor(y_test.values,dtype=torch.long)
+    test_dataset = TensorDataset(X_test_tensor,y_test_tensor)
+
+    return train_dataset, test_dataset
